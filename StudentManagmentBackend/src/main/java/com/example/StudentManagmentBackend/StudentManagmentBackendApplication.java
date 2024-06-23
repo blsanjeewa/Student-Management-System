187471670f0beac61721effd6c430c9773798b60
blsanjeewa <sandeep-ps19060@stu.kln.ac.lk>
2024-06-23 11:46:28 +0530

    initial commit

---
+++ StudentManagmentBackend/src/main/java/com/example/StudentManagmentBackend/StudentManagmentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudentManagmentBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagmentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagmentBackendApplication.class, args);
+	}
+
+}
---
+++ StudentManagmentBackend/src/test/java/com/example/StudentManagmentBackend/StudentManagmentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudentManagmentBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagmentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
